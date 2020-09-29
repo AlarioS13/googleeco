44db699323898ff0d3770852adcdbc7d32a8ce11
AlarioS13 <sofienalario@gmail.com>
2020-09-29 16:24:35 +0200

    testfinaux1

--- main.js
+++ main.js
@@ -1,2 +1,6 @@
-ScrollReveal({ distance: '200px' }); 
-ScrollReveal().reveal('.headline', { origin: 'top' }); 
+// ScrollReveal({ distance: '200px' }); 
+ScrollReveal().reveal('.headline', {distance: '200px', duration : 2000, origin: 'top' }); 
+
+
+ScrollReveal().reveal('.photo', { delay : 250, duration : 3000 }); 
+
